8fb23410901d9422084d654cad44ed273058f4a6
MasihullahMuhammadi <92782741+MasihMuhammadi@users.noreply.github.com>
2022-07-03 01:15:39 +0430

    Update header.jsx

--- src/header.jsx
+++ src/header.jsx
@@ -6,11 +6,11 @@ const Header = () =>{
        <div className=" ">
         <div className=" d-inline mx-5"><img className="logo" src="./logo.svg" alt="squad-game logo" /></div> 
       <div className="text-center">
-        <a className="mx-3" href="http://www.linkedin.com/in/MasihMuhammadi"><img width="40"src="./linkedin.svg" /></a>
+        <a className="mx-3" href="http://www.linkedin.com/in/masih-muhammadi-7a1275233"><img width="40"src="./linkedin.svg" /></a>
 
-        <a className="mx-3 " href="http://www.instagram.com/ma3ih_qara202" ><img src="./instagram.svg" /></a>
+        <a className="mx-3 " href="http://www.instagram.com/ma3_ih.qara202" ><img src="./instagram.svg" /></a>
     </div>
-        <div className=""><a href="http://www.github.com/MasihMuhammadi/squad-game"><img className="github" src="./github.svg" alt="github logo" /></a></div>
+        <div className=""><a href="http://www.github.com/MasihMuhammadi/squadGame"><img className="github" src="./github.svg" alt="github logo" /></a></div>
 
        </div>
    
